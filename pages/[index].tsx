a222ae6f8a50c7adbc07b72f1fde5acc3282718e
JJMPantoja <pantojam48@gmail.com>
2022-07-14 22:04:56 -0500

    archivo readme y rutas protegidas

--- pages/[index].tsx
+++ pages/[index].tsx
@@ -1,10 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import Login from '../components/login/Login'
 import { useEffect } from 'react'
-import { useState } from 'react'
 import { useRouter } from 'next/router'
-import Employees from './Employees'
+import Employees from './employees'
 
 export default function Home() {
 
--- pages/employees.tsx
+++ pages/employees.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import EmployeesChild from '../components/employees/Employees'
+import Login from '../components/login/Login'
 
 export default function Employees() {
+  const router = useRouter();
+
+  let fakeToken = ''
+  let isValidFakeToken = false;
+
+  useEffect(() => {
+    fakeToken = JSON.parse(localStorage.getItem('fakeToken'));
+    if ( fakeToken !== null && fakeToken !== '') {
+      isValidFakeToken = true; 
+      console.log(fakeToken);
+      router.push('/employees');
+    }
+  }, [])
+  
   return (
-    <EmployeesChild />
+    <>
+    {isValidFakeToken  ? <EmployeesChild /> : <Login />}
+    </>
+    
   )
 }
