4c98ca91d3d45a203483646c51ee8968b0ab7a16
JJMPantoja <pantojam48@gmail.com>
2022-07-18 08:17:04 -0500

    borrado imagenes

--- components/cargar/Upload.tsx
+++ components/cargar/Upload.tsx
@@ -4,14 +4,7 @@ import { FileUploader } from "react-drag-drop-files";
 export default function UploadChild() {
   const fileTypes = ["JPG", "PNG", "JPEG"];
   const [images, setImages] = useState([]);
-  // let imagenes = [];
-  let imagenes = [
-    './img/img1.jpg',
-    './img/img2.jpg',
-    './img/img3.jpg',
-    './img/img4.jpg',
-    './img/img5.jpg',
-  ]
+  let imagenes = [];
 
 
   const handleChange = (file) => {
